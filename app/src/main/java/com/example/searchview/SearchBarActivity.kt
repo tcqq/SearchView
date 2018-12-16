62f04f0c3222749cc0a4acc5d2a863eb9544d549
tcqq <usertcqq@gmail.com>
2018-12-17 02:30:43 +0800

    Add Licensed to all files

--- app/src/main/java/com/example/searchview/SearchBarActivity.kt
+++ app/src/main/java/com/example/searchview/SearchBarActivity.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2018 Martin Lapis, Alan Dreamer
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.searchview
 
 import android.content.Intent
@@ -6,9 +22,7 @@ import android.speech.RecognizerIntent
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.tcqq.searchview.Search
-import com.tcqq.searchview.graphics.SearchArrowDrawable
 import kotlinx.android.synthetic.main.activity_searchbar.*
-import kotlinx.android.synthetic.main.activity_searchview.*
 import timber.log.Timber
 
 /**
